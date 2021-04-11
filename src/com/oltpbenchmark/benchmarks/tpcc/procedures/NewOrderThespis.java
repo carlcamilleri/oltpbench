c0a68e8e305eb01b0d3e17bf2f0910955d32f427
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-04-11 14:26:13 +0200

    optimising http calls

--- src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
+++ src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
@@ -200,9 +200,9 @@ public class NewOrderThespis extends TPCCProcedure {
 			//var strRes = res.join();
 			//LOG.debug(strRes);
 
-			var futGetCust = stmtGetCustURI.execute(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
+			//var futGetCust = stmtGetCustURI.execute(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
 			var futGetWhse = stmtGetWhseURI.execute(new String[]{String.valueOf(w_id)});
-			var futGetDist = stmtGetDistURI.execute(String.valueOf(w_id),String.valueOf(d_id));
+			//var futGetDist = stmtGetDistURI.execute(String.valueOf(w_id),String.valueOf(d_id));
 
 //
 //			HttpResponse httpRespGetCust  = futGetCust.get();
@@ -220,54 +220,55 @@ public class NewOrderThespis extends TPCCProcedure {
 ////				} catch (ExecutionException e) {
 ////					throw new RuntimeException(e.getMessage());
 ////				}
-			CompletableFuture.allOf(futGetCust,futGetWhse,futGetDist).join();
+			//CompletableFuture.allOf(futGetCust,futGetWhse,futGetDist).join();
+			CompletableFuture.allOf(futGetWhse).join();
 
 //
 //
 
-
-			var results = Stream.of(futGetCust,futGetWhse,futGetDist).map(x-> {
-				try {
-					return x.get();
-
-				} catch (InterruptedException | ExecutionException e) {
-					throw new RuntimeException(e.getMessage());
-
-				}
-
-
-			}
-			).collect(Collectors.toList());
-
-			var resGetCust = results.get(0);
-			var resGetWhse = results.get(1);
-			var resGetDist = results.get(2);
-
-			var jarrGetCust = new JSONObject(resGetCust).getJSONArray("entities");
-
-			if(jarrGetCust.length()!=1)
-				throw new RuntimeException("Invalid response: "+resGetCust);
-
-			var custObj = jarrGetCust.getJSONObject(0).getJSONObject("data");
-			c_discount = Float.parseFloat(custObj.get("c_discount").toString());
-			c_last = custObj.get("c_last").toString();
-			c_credit = custObj.get("c_credit").toString();
-
-
-			var jarrGetWhse = new JSONObject(resGetWhse).getJSONArray("entities");
-			if(jarrGetWhse.length()!=1)
-				throw new RuntimeException("Invalid response: "+resGetWhse);
-
-			var whseObj = jarrGetWhse.getJSONObject(0).getJSONObject("data");
-			w_tax = Float.parseFloat(whseObj.get("w_tax").toString());
-
-			var jarrGetDist = new JSONObject(resGetDist).getJSONArray("entities");
-			if(jarrGetDist.length()!=1)
-				throw new RuntimeException("Invalid response: "+resGetDist);
-
-			var distObj = jarrGetDist.getJSONObject(0).getJSONObject("data");
-			d_next_o_id = Integer.parseInt(distObj.get("d_next_o_id").toString());
-			d_tax = Float.parseFloat(distObj.get("d_tax").toString());
+//
+//			var results = Stream.of(futGetCust,futGetWhse,futGetDist).map(x-> {
+//				try {
+//					return x.get();
+//
+//				} catch (InterruptedException | ExecutionException e) {
+//					throw new RuntimeException(e.getMessage());
+//
+//				}
+//
+//
+//			}
+//			).collect(Collectors.toList());
+//
+//			var resGetCust = results.get(0);
+//			var resGetWhse = results.get(1);
+//			var resGetDist = results.get(2);
+//
+//			var jarrGetCust = new JSONObject(resGetCust).getJSONArray("entities");
+//
+//			if(jarrGetCust.length()!=1)
+//				throw new RuntimeException("Invalid response: "+resGetCust);
+//
+//			var custObj = jarrGetCust.getJSONObject(0).getJSONObject("data");
+//			c_discount = Float.parseFloat(custObj.get("c_discount").toString());
+//			c_last = custObj.get("c_last").toString();
+//			c_credit = custObj.get("c_credit").toString();
+//
+//
+//			var jarrGetWhse = new JSONObject(resGetWhse).getJSONArray("entities");
+//			if(jarrGetWhse.length()!=1)
+//				throw new RuntimeException("Invalid response: "+resGetWhse);
+//
+//			var whseObj = jarrGetWhse.getJSONObject(0).getJSONObject("data");
+//			w_tax = Float.parseFloat(whseObj.get("w_tax").toString());
+//
+//			var jarrGetDist = new JSONObject(resGetDist).getJSONArray("entities");
+//			if(jarrGetDist.length()!=1)
+//				throw new RuntimeException("Invalid response: "+resGetDist);
+//
+//			var distObj = jarrGetDist.getJSONObject(0).getJSONObject("data");
+//			d_next_o_id = Integer.parseInt(distObj.get("d_next_o_id").toString());
+//			d_tax = Float.parseFloat(distObj.get("d_tax").toString());
 
 
 //						d_next_o_id = rs.getInt("D_NEXT_O_ID");
@@ -483,7 +484,7 @@ public class NewOrderThespis extends TPCCProcedure {
 //
 //			total_amount *= (1 + w_tax + d_tax) * (1 - c_discount);
 		}// catch(Procedure.UserAbortException | JSONException userEx)
-		catch(UserAbortException | JSONException userEx)
+		catch(UserAbortException userEx)
 		{
 		    LOG.error("Caught an expected error in New Order");
 		    throw new RuntimeException(userEx);
