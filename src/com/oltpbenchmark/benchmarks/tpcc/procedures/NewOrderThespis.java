11ecfa31fe6d46b538667909903e22c5a6bbc533
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-04-11 17:00:18 +0200

    optimising http calls

--- src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
+++ src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
@@ -230,40 +230,38 @@ public class NewOrderThespis extends TPCCProcedure {
 				return;
 			}
 
-//			var futGetCust =
-//					CompletableFuture.supplyAsync(() -> {
-//						return stmtGetCustURI.executeSync(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
-//					}, pool);
-//
-//			var futGetWhse =
-//					CompletableFuture.supplyAsync(() -> {
-//						return stmtGetWhseURI.executeSync(new String[]{String.valueOf(w_id)});
-//					}, pool);
-//
-//			var futGetDist =
-//					CompletableFuture.supplyAsync(() -> {
-//						return stmtGetDistURI.executeSync(String.valueOf(w_id),String.valueOf(d_id));
-//					}, pool);
+			var futGetCust =
+					CompletableFuture.supplyAsync(() -> {
+						return stmtGetCustURI.executeSync(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
+					}, pool);
+
+			var futGetWhse =
+					CompletableFuture.supplyAsync(() -> {
+						return stmtGetWhseURI.executeSync(new String[]{String.valueOf(w_id)});
+					}, pool);
+
+			var futGetDist =
+					CompletableFuture.supplyAsync(() -> {
+						return stmtGetDistURI.executeSync(String.valueOf(w_id),String.valueOf(d_id));
+					}, pool);
 
 			//var futGetCust = stmtGetCustURI.execute(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
-
 			///var futGetWhse =stmtGetWhseURI.execute(new String[]{String.valueOf(w_id)});
-
 			//var futGetDist =stmtGetDistURI.execute(String.valueOf(w_id),String.valueOf(d_id));
 
 
-//					var results = Stream.of(futGetCust, futGetWhse, futGetDist)
-//					.map(CompletableFuture::join).collect(Collectors.toList());
+					var results = Stream.of(futGetCust, futGetWhse, futGetDist)
+					.map(CompletableFuture::join).collect(Collectors.toList());
+
+			var resGetCust = results.get(0);
+			var resGetWhse = results.get(1);
+			var resGetDist = results.get(2);
+
+
+//			var resGetWhse = stmtGetWhseURI.executeSync(new String[]{String.valueOf(w_id)});
+//			var resGetDist = stmtGetDistURI.executeSync(String.valueOf(w_id),String.valueOf(d_id));
+//			var resGetCust = stmtGetCustURI.executeSync(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
 //
-//			var resGetCust = results.get(0);
-//			var resGetWhse = results.get(1);
-//			var resGetDist = results.get(2);
-
-
-			var resGetWhse = stmtGetWhseURI.executeSync(new String[]{String.valueOf(w_id)});
-			var resGetDist = stmtGetDistURI.executeSync(String.valueOf(w_id),String.valueOf(d_id));
-			var resGetCust = stmtGetCustURI.executeSync(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
-
 
 
 //
