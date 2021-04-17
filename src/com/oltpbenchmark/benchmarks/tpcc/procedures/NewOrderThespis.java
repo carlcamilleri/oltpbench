b9c2d0bc64b6fa174b778773088f9dc0b1e93d0b
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-04-17 17:33:51 +0200

    port change

--- src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
+++ src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
@@ -206,19 +206,19 @@ public class NewOrderThespis extends TPCCProcedure {
 //			var futGetCust = stmtGetCustURI.execute(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
 //			var futGetWhse = stmtGetWhseURI.execute(new String[]{String.valueOf(w_id)});
 //			var futGetDist = stmtGetDistURI.execute(String.valueOf(w_id),String.valueOf(d_id));
-			if(false) {
+			if(true) {
 				var fut1 =
 						CompletableFuture.supplyAsync(() -> {
-							return RESTStmt.executeSync("http://10.132.0.21:30002/");
+							return RESTStmt.executeSync("http://10.132.0.26:5000/");
 						}, pool);
 
-				var fut2 =
-						CompletableFuture.supplyAsync(() -> {
-							return RESTStmt.executeSync("http://10.132.0.21:30002/");
-						}, pool);
+//				var fut2 =
+//						CompletableFuture.supplyAsync(() -> {
+//							return RESTStmt.executeSync("http://10.132.0.21:30002/");
+//						}, pool);
 
-				var r = Stream.of(fut1,fut2)
-						.map(CompletableFuture::join).collect(Collectors.toList());
+//				var r = Stream.of(fut1,fut2)
+//						.map(CompletableFuture::join).collect(Collectors.toList());
 
 				return;
 			}
