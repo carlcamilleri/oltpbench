c3bb045f0c85112d1cab722dc1048881163e3921
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-04-25 10:49:38 +0200

    port change

--- src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
+++ src/com/oltpbenchmark/benchmarks/tpcc/procedures/NewOrderThespis.java
@@ -206,7 +206,7 @@ public class NewOrderThespis extends TPCCProcedure {
 //			var futGetCust = stmtGetCustURI.execute(String.valueOf(w_id),String.valueOf(d_id),String.valueOf(c_id));
 //			var futGetWhse = stmtGetWhseURI.execute(new String[]{String.valueOf(w_id)});
 //			var futGetDist = stmtGetDistURI.execute(String.valueOf(w_id),String.valueOf(d_id));
-			if(true) {
+			if(false) {
 				var fut1 =
 						CompletableFuture.supplyAsync(() -> {
 							return RESTStmt.executeSync("http://10.132.0.34:30002/");
@@ -224,6 +224,14 @@ public class NewOrderThespis extends TPCCProcedure {
 
 				var r = Stream.of(fut1,fut2,fut3)
 						.map(CompletableFuture::join).collect(Collectors.toList());
+
+				return;
+			}
+
+			if(true) {
+				RESTStmt.executeSync("http://10.132.0.34:30002/");
+				RESTStmt.executeSync("http://10.132.0.35:30002/");
+				RESTStmt.executeSync("http://10.132.0.33:30002/");
 
 				return;
 			}
