da7e107ff7c0b605a406c274abd0685dfd10ffd6
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-07-18 11:41:36 +0200

    ycsb for Thespis - perf opt

--- src/com/oltpbenchmark/benchmarks/ycsb/YCSBWorkerFactory.java
+++ src/com/oltpbenchmark/benchmarks/ycsb/YCSBWorkerFactory.java
@@ -5,11 +5,18 @@ import com.oltpbenchmark.api.Worker;
 import java.sql.SQLException;
 
 public class YCSBWorkerFactory {
+    private static String thespisUrl = "n/a";
+
     public static Worker<YCSBBenchmark> createWorker(YCSBBenchmark benchmarkModule, int id,
                                                      int init_record_count) throws SQLException {
         if (benchmarkModule.getWorkloadConfiguration().getBenchmarkName().equalsIgnoreCase("ycsb_thespis")) {
-            var thespisUrl = benchmarkModule.getWorkloadConfiguration().getXmlConfig().getDocument().getElementsByTagName("thespisUrl").item(0).getTextContent();
-            return new YCSBWorkerThespis(benchmarkModule, id, init_record_count,thespisUrl);
+            if(YCSBWorkerFactory.thespisUrl.equalsIgnoreCase("n/a")) {
+                synchronized (YCSBWorkerFactory.class) {
+                    YCSBWorkerFactory.thespisUrl = benchmarkModule.getWorkloadConfiguration().getXmlConfig().getDocument().getElementsByTagName("thespisUrl").item(0).getTextContent();
+                }
+            }
+
+            return new YCSBWorkerThespis(benchmarkModule, id, init_record_count,YCSBWorkerFactory.thespisUrl);
         } else {
             return new YCSBWorker(benchmarkModule, id, init_record_count);
         }
