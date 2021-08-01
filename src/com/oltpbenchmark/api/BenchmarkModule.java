a50fa7f957b404c717fa3f989cab89acd6323695
Carl Camilleri <carl.camilleri@cs-technologies.net>
2021-08-01 15:25:55 +0200

    Revert "YCSB - connection parameters"
    This reverts commit a2163169726f571f8ea72035b74c7a582d2b922f.

--- src/com/oltpbenchmark/api/BenchmarkModule.java
+++ src/com/oltpbenchmark/api/BenchmarkModule.java
@@ -113,19 +113,11 @@ public abstract class BenchmarkModule {
      * @throws SQLException
      */
     public final Connection makeConnection() throws SQLException {
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
         Connection conn = DriverManager.getConnection(
                 workConf.getDBConnection(),
                 workConf.getDBUsername(),
                 workConf.getDBPassword());
         Catalog.setSeparator(conn);
-
         return (conn);
     }
 
@@ -435,30 +427,40 @@ public abstract class BenchmarkModule {
      * @return
      */
     public Map<TransactionType, Procedure> getProcedures() {
-        Map<TransactionType, Procedure> proc_xref = new HashMap<TransactionType, Procedure>();
-        TransactionTypes txns = this.workConf.getTransTypes();
+        //Map<TransactionType, Procedure> proc_xref = new HashMap<TransactionType, Procedure>();
+        if(proc_xref==null) {
+            synchronized (BenchmarkModule.class) {
+                if(proc_xref==null) {
 
-        if (txns != null) {
-            for (Class<? extends Procedure> procClass : this.supplementalProcedures) {
-                TransactionType txn = txns.getType(procClass);
-                if (txn == null) {
-                    txn = new TransactionType(procClass, procClass.hashCode(), true);
-                    txns.add(txn);
+                   var procXref = new  HashMap<TransactionType, Procedure>();
+                    TransactionTypes txns = this.workConf.getTransTypes();
+
+                    if (txns != null) {
+                        for (Class<? extends Procedure> procClass : this.supplementalProcedures) {
+                            TransactionType txn = txns.getType(procClass);
+                            if (txn == null) {
+                                txn = new TransactionType(procClass, procClass.hashCode(), true);
+                                txns.add(txn);
+                            }
+                        } // FOR
+
+                        for (TransactionType txn : txns) {
+                            Procedure proc = (Procedure) ClassUtil.newInstance(txn.getProcedureClass(),
+                                    new Object[0],
+                                    new Class<?>[0]);
+                            proc.initialize(this.workConf.getDBType());
+                            procXref.put(txn, proc);
+                            proc.loadSQLDialect(this.dialects);
+                        } // FOR
+                    }
+                    if (procXref.isEmpty()) {
+                        LOG.warn("No procedures defined for " + this);
+                    }
+                    proc_xref = procXref;
                 }
-            } // FOR
+            }
+        }
 
-            for (TransactionType txn : txns) {
-                Procedure proc = (Procedure)ClassUtil.newInstance(txn.getProcedureClass(),
-                        new Object[0],
-                        new Class<?>[0]);
-                proc.initialize(this.workConf.getDBType());
-                proc_xref.put(txn, proc);
-                proc.loadSQLDialect(this.dialects);
-            } // FOR
-        }
-        if (proc_xref.isEmpty()) {
-            LOG.warn("No procedures defined for " + this);
-        }
         return (proc_xref);
     }
 
